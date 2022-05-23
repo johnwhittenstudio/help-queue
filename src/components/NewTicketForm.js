6e202fa01fac3e3227dda516bb44fe3709edeca9
John Whitten <johnwhitten.studio@gmail.com>
2022-05-23 16:51:39 -0700

    pass data via callbacks

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { v4 } from 'uuid';
+import PropTypes from "prop-types";
 
-function NewTicketForm(){
+function NewTicketForm(props){
 
   function handleNewTicketFormSubmission(event) {
     event.preventDefault();
-    console.log(event.target.names.value);
-    console.log(event.target.location.value);
-    console.log(event.target.issue.value);
+    props.onNewTicketCreation({names: event.target.names.value, locations: event.target.location.value, issue: event.target.issue.value, id: v4()});
   }
 
   return (
     <React.Fragment>
+      <div class="container">
+        <div class="row">
       <form onSubmit={handleNewTicketFormSubmission}>
         <input
           type='text'
@@ -24,10 +25,16 @@ function NewTicketForm(){
         <textarea
           name='issue'
           placeholder='Describe your issue.' />
-        <button type='submit'>Help!</button>
+        <button id="button-help" type='submit'>Help!</button>
       </form>
+      </div>
+      </div>
     </React.Fragment>
   );
 }
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewTicketForm;
